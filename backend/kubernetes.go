1023c4b0db44d2e6d8978c0a66f33059fa8beefa
Adolphe Cher-Aime <acheraime@powerchordsystem.com>
2021-03-17 15:57:14 -0400

    Add func to build gcp specific configuration

--- backend/kubernetes.go
+++ backend/kubernetes.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 
 	"google.golang.org/api/container/v1"
@@ -10,12 +11,13 @@ import (
 )
 
 type KubernetesBackend struct {
-	Type       TLSBackendType
-	NameSpace  string
-	K8sContext string
-	ProjectID  string
-	Provider   string
-	client     *kubernetes.Clientset
+	Type          TLSBackendType
+	NameSpace     string
+	K8sContext    string
+	ProjectID     string
+	CloudProvider string
+	client        *kubernetes.Clientset
+	config        *api.Config
 }
 
 func NewK8sBackend() (Backend, error) {
@@ -27,6 +29,8 @@ func NewK8sBackend() (Backend, error) {
 }
 
 func (k KubernetesBackend) build() error {
+	ctx := context.Background()
+
 	return nil
 }
 
@@ -35,13 +39,65 @@ func (k KubernetesBackend) Publish() error {
 	return nil
 }
 
-func getk8sconfig(ctx context.Context) (*api.Config, error) {
+func (k KubernetesBackend) getk8sconfig(ctx context.Context) (*api.Config, error) {
 	svc, err := container.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Bare bone configuration structure
+	config := api.Config{
+		APIVersion: "v1",
+		Kind:       "Config",
+		Clusters:   map[string]*api.Cluster{},
+		AuthInfos:  map[string]*api.AuthInfo{},
+		Contexts:   map[string]*api.Context{},
+	}
+
+	switch k.CloudProvider {
+	case "gcp":
+
+	}
+	return &config, nil
+}
+
+func getK8sclient(conf api.Config)
+
+func (k *KubernetesBackend) buildGCPConfig(ctx context.Context, svc *container.Service) error {
+	// Retrieve the list of gke clusters for the project ID
+	res, err := svc.Projects.Zones.Clusters.List(k.ProjectID, "-").Context(ctx).Do()
 	if err != nil {
 		return err
 	}
 
-	return nil, nil
-}
+	// Iterate over the clusters and start populating the api
+	for _, c := range res.Clusters {
+		// Cluster name
+		clusterName := fmt.Sprintf("gke_%s_%s_%s", k.ProjectID, c.Zone, c.Name)
+		cert, err := base64.StdEncoding.DecodeString(c.MasterAuth.ClientCertificate)
+		if err != nil {
+			return err
+		}
+		// Populate the config object with information from this cluster
+		k.config.Clusters[clusterName] = &api.Cluster{
+			CertificateAuthorityData: cert,
+			Server:                   "https://" + c.Endpoint,
+		}
+		// Contexts
+		k.config.Contexts[clusterName] = &api.Context{
+			Cluster:  clusterName,
+			AuthInfo: clusterName,
+		}
 
-func getK8sclient(conf api.Config)
+		k.config.AuthInfos[clusterName] = &api.AuthInfo{
+			AuthProvider: &api.AuthProviderConfig{
+				Name: "gcp",
+				Config: map[string]string{
+					"scopes": "https://www.googleapis.com/auth/cloud-platform",
+				},
+			},
+		}
+	}
+
+	return nil
+}
