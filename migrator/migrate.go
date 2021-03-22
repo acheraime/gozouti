d5066f8502096c0b3d7ee9d3984dec296114e4b7
Adolphe Cher-Aime <acheraime@powerchordsystem.com>
2021-03-22 18:34:32 -0400

    Add readfile helper func

--- migrator/migrate.go
+++ migrator/migrate.go
@@ -1,5 +1,27 @@
 package migrator
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
 func NewMigration() error {
 	return nil
+}
+
+func ReadFile(inFile string) (*os.File, error) {
+	if _, err := os.Stat(inFile); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("%s does not exist", inFile)
+		}
+	}
+	// Open the file
+	file, err := os.Open(inFile)
+	if err != nil {
+		fmt.Printf("attempt to open csv file %s failed: %v", inFile, err)
+		return nil, err
+	}
+
+	return file, nil
 }
